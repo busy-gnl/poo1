bf6d1f866fd183d9ee3c3233c0c9e1fd22ef86e6
busy-gnl <vallantjesse@live.com>
2022-11-07 19:08:03 +0100

    completed basics6

--- Class/Speedometer.php
+++ Class/Speedometer.php
@@ -4,13 +4,13 @@ class Speedometer
 {
     public const DISTANCE_CONVERSION = 1.609;
 
-    public function convertKmToMiles(int $nbMiles)
+    public function convertKmToMiles(int $nbMiles): float
     {
         return $nbMiles / self::DISTANCE_CONVERSION;
     }
 
-    public function convertMilesToKm(int $nbMiles)
+    public function convertMilesToKm(int $nbKm): float
     {
-        return $nbMiles * self::DISTANCE_CONVERSION;
+        return $nbKm * self::DISTANCE_CONVERSION;
     }
 }
